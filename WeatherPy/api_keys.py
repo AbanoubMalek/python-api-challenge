0b4fc1da25de996c03901467bbf9bd77e025b520
AbanoubMalek <abanoubmalek1@gmail.com>
2024-02-20 18:52:11 -0500

    git ignore

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,4 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
-
+weather_api_key ="b73debaf687a8c04d4f5f1431fde3b31"
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key ="cd4c65b1edc46f799ddbe75ed97f5c3"
